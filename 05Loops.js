b68ac76ce15b12dc4bff20f2a69dd35c660b41d3
ak011125 <ajaykumarjakhar08@gmail.com>
2025-12-26 15:50:15 +0530

    2nd

---
+++ 05Loops.js
@@ -0,0 +1,12 @@
+for (let i =1; i<= 5; i++){
+    console.log("Hello world");
+}
+for(let i =0; i<5;i++){
+    console.log("Hii");
+}
+let sum = 0;
+for(let i = 0;i < 5; i++){
+    sum = sum + i;
+    console.log(sum);
+}
+console.log("Net sum = ",sum);
