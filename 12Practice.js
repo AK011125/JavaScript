be7a068d71e2cc110eaac051b747cd114c1abbe9
ak011125 <ajaykumarjakhar08@gmail.com>
2026-01-02 10:51:23 +0530

    12th

---
+++ 12Practice.js
@@ -0,0 +1,6 @@
+let companies = ["Google","Microsoft","Uber","IBM","Netflix","Bloomberg"];
+console.log(companies);
+companies.shift();
+console.log(companies);
+companies.splice(2,1,"Ola");
+console.log(companies);
