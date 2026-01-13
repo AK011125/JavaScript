27cc9727f0fccbed925184f4a2232fa9e9eb6d28
ak011125 <ajaykumarjakhar08@gmail.com>
2026-01-13 22:24:26 +0530

    21st

---
+++ JS 2/21DOM.js
@@ -0,0 +1,9 @@
+let para = document.querySelector("p");
+console.log(para.setAttribute("class","newClass"));
+
+// para.setAttribute("class","123")
+
+let div = document.querySelector("div");
+div.style.backgroundColor = "green";
+div.style.fontSize = "26px";
+div.innerText = "Hello!!";
