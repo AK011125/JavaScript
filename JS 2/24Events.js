84b30dde5438a2de73e89aa1ce2532d58421acc7
ak011125 <ajaykumarjakhar08@gmail.com>
2026-01-14 23:39:26 +0530

    27th

---
+++ JS 2/24Events.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".btn1");
+
+btn.onclick = () => {
+    console.log("button was clicked");
+}
+//here inline have lower priority then js sheet
---
+++ JS 2/25Event2.js
@@ -0,0 +1,33 @@
+let div = document.querySelector("div");
+
+div.onmouseover = () =>{
+    console.log("You are in div")
+};
+
+//event object
+div.onmouseover = (e) =>{
+    console.log(e); //event ki info
+    console.log(e.type); // kya karne se event hua
+    console.log(e.target); //kiske upar event hua
+    console.log(e.clientX, e.clientY); //x and y corrodinates of event
+
+};
+
+//Event listners
+let btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click", () =>{   // we can also pass event object here (e)
+    console.log("button was clicked");
+});
+// they can add two or more task at same time
+btn1.addEventListener("click", () =>{
+    console.log("button was clicked 2");
+});
+
+btn1.addEventListener("click", () =>{
+    console.log("button was clicked 3");
+});
+
+btn1.addEventListener("click", () =>{
+    console.log("button was clicked 4");
+});
+// to remove event listner
