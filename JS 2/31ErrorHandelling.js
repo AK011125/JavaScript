2ee1222dcf725b3f02817df078bebd2ff065706e
ak011125 <ajaykumarjakhar08@gmail.com>
2026-01-18 17:32:39 +0530

    Error Handelling

---
+++ JS 2/31ErrorHandelling.js
@@ -0,0 +1,15 @@
+let a = 4;
+let b = 56;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+// console.log(a+c); //error 
+// so we put it in try catch it shows what is the problem
+
+try{
+    console.log(a+c);
+}catch(err){
+    console.log(err);
+}
+
+console.log(a/b);
