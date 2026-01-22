e3c83002159cae7b0819e392cbdef513213cee9d
ak011125 <ajaykumarjakhar08@gmail.com>
2026-01-22 22:24:58 +0530

    Async function on API

---
+++ JS 2/36APi.js
@@ -0,0 +1,15 @@
+const URL = "https://poetrydb.org/title/Ozymandias/lines.json";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
+
+
+const getFacts = async() =>{
+    console.log("Getting data ....");
+    let response = await fetch(URL);
+    console.log(response); //JSON format
+    let data = await response.json();
+    factPara.innerText = data[0].text;
+};
+
+
+btn.addEventListener("click", getFacts);
